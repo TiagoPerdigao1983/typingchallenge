fdd5be799252b83a70261544ec9cc76b38643d18
Tiago Perdigao <tiago.perdigao@hotmail.com>
2021-11-13 14:07:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    console.log(value);
+    this.enteredText = value;
+  }
 }
+
+//display each character in the template, but color green if it's right, red if it's wrong and gray if a given character hasn't been entered yet
